723188c20c700db8e141fa1db1bd630dfb1583f7
santy-101 <santiago.lema01@epn.edu.ec>
2017-03-03 18:44:50 -0500

    2

--- AplicacionBodega/AplicacionBodega/src/app/services/master-url.service.ts
+++ AplicacionBodega/AplicacionBodega/src/app/services/master-url.service.ts
@@ -6,7 +6,7 @@ export class MasterURlService {
 
   constructor() {
    // this._url = "http://localhost:1337/";
-    this.url = "http://examen-twj-lemasantiago-santiagolema.c9users.io:8080"
+    this._url = "http://examen-twj-lemasantiago-santiagolema.c9users.io:8080/"
   }
 
   get url():string{
